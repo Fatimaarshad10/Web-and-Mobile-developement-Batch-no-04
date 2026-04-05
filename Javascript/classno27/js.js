3b570ea557d4348dff2f5fecd5c7a8e281f671eb
Fatimaarshad10 <fatimaarshad118@gmail.com>
2026-04-05 15:49:16 +0500

    class no 40

---
+++ Javascript/classno27/js.js
@@ -0,0 +1,50 @@
+function Subtract(a, b) {
+    return a - b
+}
+// parameter -> variable 
+// arguments -> values
+
+// const output = Subtract(10,30)
+// console.log(Subtract(10,30))
+// Subtract(30,20)
+
+// const multiply = function(a, b) {
+//   return a * b;
+// };
+
+// let output = multiply(20,2)
+// console.log(output)
+
+// const multiply = a => {
+//   return a * b;
+// };
+
+// function sayHello() {
+//   return;
+// }
+
+// let text = sayHello();
+
+// console.log(text)
+
+// add()
+// function add(){
+//     console.log('hello')
+// }
+
+// add()
+// var add = function(){
+//     console.log("werwe")
+// }
+
+// const add = (a, b) => return (a + b);
+// const add = (a, b) => a + b;
+
+
+// console.log(carName)
+// {
+//     const carName = "Volvo";
+
+
+// }
+// console.log(carName)
---
+++ Javascript/classno29/js.js
@@ -0,0 +1,39 @@
+// let student1 = [{name : "amna"}]
+// JSON -> Javascript Object Notation
+// SQL , NON SQL
+// fetch('https://jsonplaceholder.typicode.com/posts')
+
+//  index = 0 
+// length = 1 
+
+// console.log(student[0])
+// console.log(student[1])
+// console.log(student[2])
+// console.log(student[3])
+// console.log(student[4])
+// console.log(student[5])
+// console.log(student.length)
+
+let student = ["amna" , 24, 2323 , null , false]
+// student.pop() remove from end 
+// student.pop()
+// student.pop()
+// student.shift() remove from start
+
+// student.push(["fatima" , 32323])
+
+// console.log(student[5][0])
+
+// student.push('fatima') add end
+// student.unshift("fatima") start add 
+// console.log(student.toString())
+// console.log(student.at(0))
+
+// for(let i = 0; i < student.length; i++){
+//     if(student[i] === 'amna'){
+//         student.shift()
+//     }
+//     console.log(student[i])
+
+// }
+// console.log(student)
